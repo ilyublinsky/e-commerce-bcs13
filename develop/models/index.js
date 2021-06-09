0f71818e68626aaca11049b36b1e484b09dcc728
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-06-08 19:59:01 -0700

    completed index in model

--- develop/models/index.js
+++ develop/models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreign_key: 'id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_key:'id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreign_key:'id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreign_key:'tag_id'
+})
 
 module.exports = {
   Product,
